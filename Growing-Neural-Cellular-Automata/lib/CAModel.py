d2702385274efb35190cd53f7fa2a6d1152d57b0
Filipe <filipe.rolim@gmail.com>
2024-05-07 08:51:07 -0300

    added gitignore

--- Growing-Neural-Cellular-Automata/lib/CAModel.py
+++ Growing-Neural-Cellular-Automata/lib/CAModel.py
@@ -724,6 +724,88 @@ class mCAModel_v8(nn.Module):
         for step in range(steps):
             x = self.update(x, fire_rate, angle)
         return x
+
+class mCAModel_v9(nn.Module):
+    def __init__(self, channel_n, fire_rate, device, hidden_size=128):
+        super(mCAModel_v9, self).__init__()
+
+        self.device = device
+        self.channel_n = channel_n
+
+        self.fc0 = nn.Linear(channel_n*2, hidden_size)
+        # self.fc0 = nn.Linear(channel_n*5, hidden_size)
+        self.fc1 = nn.Linear(hidden_size, channel_n, bias=False)
+        #self.fc1 = nn.Linear(channel_n*5, channel_n, bias=False)
+        with torch.no_grad():
+            self.fc1.weight.zero_()
+
+        self.fire_rate = fire_rate
+        self.to(self.device)
+
+    def alive(self, x):
+        return F.max_pool2d(x[:, 3:4, :, :], kernel_size=3, stride=1, padding=1) > 0.1
+
+    def perceive(self, x, angle):
+
+        def _perceive_with(x, weight):
+            conv_weights = torch.from_numpy(weight.astype(np.float32)).to(self.device)
+            conv_weights = conv_weights.view(1,1,3,3).repeat(self.channel_n, 1, 1, 1)
+            return F.conv2d(x, conv_weights, padding=1, groups=self.channel_n)
+
+        #dx = np.outer([1, 2, 1], [-1, 0, 1]) / 8.0  # Sobel filter
+        dx = np.outer([1, 2, 1], [-1, 0, 1])   # Sobel filter
+        
+        #dlap = np.array([[1, 2, 1], [2, -12, 2],[1,2,1]]) / 8.0  # Sobel filter
+        #dlap = np.array([[1, 2, 1], [2, -12, 2],[1,2,1]])   # Sobel filter
+        dlap2 = np.array([[0, 1, 0], [1, -4, 1],[0,1,0]]) / 8.0  # Sobel filter
+        # dlap2 = np.array([[0, 1, 0], [1, -4, 1],[0,1,0]])   # Sobel filter
+        #dlap2 = np.array([[0, 1, 0], [1, -4, 1],[0,1,0]])   # Sobel filter
+        dy = dx.T
+        c = np.cos(angle*np.pi/180)
+        s = np.sin(angle*np.pi/180)
+        w1 = c*dx-s*dy
+        w2 = s*dx+c*dy
+
+        y1 = _perceive_with(x, w1)
+        y2 = _perceive_with(x, w2)
+        # y3 = _perceive_with(x,dlap)
+        y4 = _perceive_with(x,dlap2)
+        #y = torch.cat((x,y1,y2),1)
+        #y = torch.cat((y1,y2),1)
+        y = torch.cat((x,y1),1)
+        # y = torch.cat((x,y1,y2,y3,y4),1)
+        #y = torch.cat((x,y1,y2,y3,torch.abs(x), torch.abs(y1),torch.abs(y2), torch.abs(y3)),1)
+        # yx = torch.cat(y,torch.abs(y),1)
+        # yx = y
+        return y
+
+    def update(self, x, fire_rate, angle):
+        x = x.transpose(1,3)
+        pre_life_mask = self.alive(x)
+
+        dx = self.perceive(x, angle)
+        dx = dx.transpose(1,3)
+        dx = self.fc0(dx)
+        dx = F.relu(dx)
+        dx = self.fc1(dx)
+
+        if fire_rate is None:
+            fire_rate=self.fire_rate
+        stochastic = torch.rand([dx.size(0),dx.size(1),dx.size(2),1])>fire_rate
+        stochastic = stochastic.float().to(self.device)
+        dx = dx * stochastic
+
+        x = x+dx.transpose(1,3)
+
+        post_life_mask = self.alive(x)
+        life_mask = (pre_life_mask & post_life_mask).float()
+        x = x * life_mask
+        return x.transpose(1,3)
+
+    def forward(self, x, steps=1, fire_rate=None, angle=0.0):
+        for step in range(steps):
+            x = self.update(x, fire_rate, angle)
+        return x
     
 class smallModel(nn.Module):
     def __init__(self, channel_n, fire_rate, device, hidden_size=128):
