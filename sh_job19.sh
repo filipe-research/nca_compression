a951de4ef0b143c8dd910a2ecc5d226900add0d9
Filipe <filipe.rolim@gmail.com>
2024-05-23 15:06:18 -0300

    small

---
+++ sh_job19.sh
@@ -0,0 +1,4 @@
+sh sh_control_exp_compressNCA_nocur.sh coco_gnca_large_ts40_000000012214 coco_gnca_large_ts40_000000012214.yaml;
+sh sh_control_exp_compressNCA_nocur.sh coco_gnca_large_ts40_000000012272 coco_gnca_large_ts40_000000012272.yaml;
+sh sh_control_exp_compressNCA_nocur.sh coco_gnca_large_ts40_000000014696 coco_gnca_large_ts40_000000014696.yaml;
+
